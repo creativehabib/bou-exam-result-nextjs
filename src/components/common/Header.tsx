62d6d5f1bfdcdc63b635de2f17fee35c820ca53e
creativehabib <shamolrahaman@gmail.com>
2024-11-25 14:22:14 +0600

    code modify

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,20 +5,18 @@ import { justlogo, logo } from '../../../public/img'
 
 const Header = () => {
   return (
-    <div>
-        <header className="sticky top-0 z-40 border-b border-b-slate-200 bg-white/75 backdrop-blur-sm">
-          <nav className="container mx-auto px-4">
-            <div className="flex h-16 items-center justify-center">
-              <Link href="/">
-                <div className="flex items-center">
-                  <Image src={justlogo} width={55} height={55} alt="logo" className="mt-2 mr-2" />
-                  <Image src={logo} alt="logo" width={350} height={50} className='hidden md:block' />
-                </div>
-              </Link>
+    <header className="sticky top-0 z-40 border-b border-b-slate-200 bg-white/75 backdrop-blur-sm">
+        <nav className="container mx-auto px-4">
+        <div className="flex h-16 items-center justify-center">
+            <Link href="/">
+            <div className="flex items-center">
+                <Image src={justlogo} width={55} height={55} alt="logo" className="mt-2 mr-2" />
+                <Image src={logo} alt="logo" width={350} height={50} className='hidden md:block' />
             </div>
-          </nav>
-      </header>
-    </div>
+            </Link>
+        </div>
+        </nav>
+    </header>
   )
 }
 
