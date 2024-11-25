391bd6d1d24a89e54ffbe2298ce29e11b6f1b63b
creativehabib <shamolrahaman@gmail.com>
2024-11-25 12:15:51 +0600

    header logo responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,13 +103,13 @@ export default function Home() {
   
   return (
     <div className="min-h-screen flex flex-col bg-white">
-      <header className="sticky top-0 z-40 w-full border-b border-b-slate-200 bg-white/75 backdrop-blur-sm">
+      <header className="sticky top-0 z-40 border-b border-b-slate-200 bg-white/75 backdrop-blur-sm">
           <nav className="container mx-auto px-4">
             <div className="flex h-16 items-center justify-center">
               <Link href="/">
                 <div className="flex items-center">
                   <Image src={justlogo} width={55} height={55} alt="logo" className="mt-2 mr-2" />
-                  <Image src={logo} alt="logo" width={350} height={50} />
+                  <Image src={logo} alt="logo" width={350} height={50} className='hidden md:block' />
                 </div>
               </Link>
             </div>
