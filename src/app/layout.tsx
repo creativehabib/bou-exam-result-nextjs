6ba7b131542e8d77739e8b9ffe98027355ebf147
Habibur Rahaman <shamolrahaman@gmail.com>
2024-11-30 00:30:20 +0600

    Font Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto } from 'next/font/google';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const robot = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
   title: "Result | Bangladesh Open University",
@@ -18,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className} suppressHydrationWarning={true}>
+      <body className={robot.className} suppressHydrationWarning={true}>
         {children}
         <ToastContainer position='top-right'/>
       </body>
